67e2c8af9867d23a133c348a1a4f142fdf1446fc
fabistb <fabian0401@online.de>
2022-06-06 16:13:05 +0200

    introduce pubsub filter exmple

--- Main/Main/Controllers/PubSubController.cs
+++ Main/Main/Controllers/PubSubController.cs
@@ -18,18 +18,41 @@ public class PubSubController : ControllerBase
         _logger = logger;
     }
 
-    [HttpPost]
+    [HttpPost("pub-sub-send")]
     public async Task<IActionResult> PublishMessage([FromBody] PubSubMessageDto message)
     {
-        await _pubSubService.PublishMessage(message.Message);
+        await _pubSubService.PublishMessage("pub-sub-receive", message.Message);
         return Ok();
     }
-
+    
     [Topic("messagebus", "pub-sub-receive")]
     [HttpPost("pub-sub-receive")]
     public async Task<IActionResult> ReceiveMessage([FromBody] PubSubMessageDto messageDto)
     {
         _logger.LogInformation($"PubSub message: {messageDto.Message}");
+        return Ok();
+    }
+    
+    [HttpPost("pub-sub-filter")]
+    public async Task<IActionResult> PublishMessageFilter([FromBody] PubSubMessageDto message)
+    {
+        await _pubSubService.PublishMessage("pub-sub-filter", message.Message, message.Version);
+        return Ok();
+    }
+    
+    [Topic("messagebus", "pub-sub-filter", "event.data.version ==\"1\"", 1)]
+    [HttpPost("version-1")]
+    public async Task<IActionResult> ReceiveMessageVersion1([FromBody] PubSubMessageDto messageDto)
+    {
+        _logger.LogInformation($"PubSub filter message: {messageDto.Message}");
+        return Ok();
+    }
+    
+    [Topic("messagebus", "pub-sub-filter", "event.data.version ==\"2\"", 2)]
+    [HttpPost("version-2")]
+    public async Task<IActionResult> ReceiveMessageVersion2([FromBody] PubSubMessageDto messageDto)
+    {
+        _logger.LogInformation($"PubSub filter message: {messageDto.Message}");
         return Ok();
     }
 }
