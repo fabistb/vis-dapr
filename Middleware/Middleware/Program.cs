2fba008ff3b579fa823d5fde28e3bb8f2cfc08cf
fabistb <fabian0401@online.de>
2023-02-07 22:24:24 +0100

    init implementation of middleware example

---
+++ Middleware/Middleware/Program.cs
@@ -0,0 +1,36 @@
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddHttpClient();
+builder.Services.AddDaprClient();
+builder.Services.AddControllers().AddDapr();
+builder.Services.AddHealthChecks();
+
+builder.Services.AddApiVersioning(v =>
+{
+    v.ReportApiVersions = true;
+    v.AssumeDefaultVersionWhenUnspecified = true;
+    v.DefaultApiVersion = new ApiVersion(1, 0);
+});
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddDaprSidekick(builder.Configuration);
+}
+
+// Configure the HTTP request pipeline.
+var app = builder.Build();
+
+app.UseRouting();
+app.UseHttpsRedirection();
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapHealthChecks("/health");
+    endpoints.MapControllers();
+});
+
+app.Run();
